7eb9ef0ce9702c91930534640ed81ed20b9384f3
Amil Masic <amil.masic@gmail.com>
2018-09-09 16:01:50 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
+require "pry"
+def countdown(timer)
+  binding.pry
+  while timer > 0
 
-def countdown
+    puts "#{timer} SECOND(S)!"
+    timer -= 1
+    if timer == 0
+      puts "HAPPY NEW YEAR!"
+    end
+  end
 end
+
+
+
+# def countdown
+# end
