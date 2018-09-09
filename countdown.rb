94a0b7417e861d5bafbd029a41d39e76047a2d4d
Amil Masic <amil.masic@gmail.com>
2018-09-09 15:56:03 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,15 @@
 #write your code here
+def countdown
+
+  while countdown > 0
+    puts "#{countdown} SECOND(S)!"
+    if countdown == 0
+      puts "HAPPY NEW YEAR!"
+    end
+  end
+end
+
+
 
 def countdown
 end
